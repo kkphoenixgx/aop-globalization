1dfcabdf55b546c2eaadc7e5edfd090fcb66f879
Kauã Alves Santos <kauaalvesworkplace@gmail.com>
2026-08-26 17:56:22 -0300

    throws Exception error

--- sdk/java/src/main/java/io/panteao/Panteao.java
+++ sdk/java/src/main/java/io/panteao/Panteao.java
@@ -1,16 +1,16 @@
 package io.panteao;
 
 public class Panteao extends BdiClient {
-    public Panteao(String host) {
+    public Panteao(String host) throws Exception {
         super(host);
     }
-    public Panteao(String host, int port) {
+    public Panteao(String host, int port) throws Exception {
         super(host, port);
     }
-    public Panteao(String host, int port, String project) {
+    public Panteao(String host, int port, String project) throws Exception {
         super(host, port, project);
     }
-    public Panteao(String host, int port, String project, boolean dev) {
+    public Panteao(String host, int port, String project, boolean dev) throws Exception {
         super(host, port, project, dev);
     }
 }
